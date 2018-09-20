a2a48ff3367c66f346562325f4144fce73d0cb18
downerj <downer.james.256k@gmail.com>
2018-09-20 15:45:43 -0600

    Add weeks

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en-US">
+    <head>
+        <title>Jimmy Downer's Assignments</title>
+        <meta charset="UTF-8" />
+        <link rel="stylesheet" href="top.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    </head>
+    <body>
+        <div class="banner-top center-text">
+            <span class="header">CS 213 Assignments</span>
+        </div>
+        <div class="content-top">
+            <ul class="center-text">
+                <li><a href="week02/index.html">Week 02</a></li>
+                <li><a href="week03/index.html">Week 03</a></li>
+                <li><a href="week04/index.html">Week 04</a></li>
+                <li><a href="week05/index.html">Week 05</a></li>
+                <li><a href="week06/index.html">Week 06</a></li>
+                <li><a href="week07/index.html">Week 07</a></li>
+                <li><a href="week08/index.html">Week 08</a></li>
+                <li><a href="week09/index.html">Week 09</a></li>
+                <li><a href="week10/index.html">Week 10</a></li>
+                <li><a href="week11/index.html">Week 11</a></li>
+                <li><a href="week12/index.html">Week 12</a></li>
+                <li><a href="week13/index.html">Week 13</a></li>
+            </ul>
+        </div>
+    </body>
+</html>

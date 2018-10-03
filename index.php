5fd373043bcb1761fceec9cee9f73dd527ba0843
James D. Downer <dow16002@byui.edu>
2018-10-03 13:29:33 -0600

    Change base href

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
     <head>
         <title>Jimmy Downer's Assignments</title>
         <meta charset="UTF-8" />
-        <link rel="stylesheet" href="/top.css" />
+        <base href="~" />
+        <link rel="stylesheet" href="top.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     </head>
     <body>
@@ -14,18 +15,18 @@
         </div>
         <div class="content-top fill">
             <ul class="center-text">
-                <li><a href="/week02/index.html">Week 02</a><br /><sup>My WebDen</sup></li>
-                <li><a href="/week03/index.html">Week 03</a><br /><sup>E-Commerce</sup></li>
-                <li><a href="/week04/index.html">Week 04</a></li>
-                <li><a href="/week05/index.html">Week 05</a></li>
-                <li><a href="/week06/index.html">Week 06</a></li>
-                <li><a href="/week07/index.html">Week 07</a></li>
-                <li><a href="/week08/index.html">Week 08</a></li>
-                <li><a href="/week09/index.html">Week 09</a></li>
-                <li><a href="/week10/index.html">Week 10</a></li>
-                <li><a href="/week11/index.html">Week 11</a></li>
-                <li><a href="/week12/index.html">Week 12</a></li>
-                <li><a href="/week13/index.html">Week 13</a></li>
+                <li><a href="week02/index.html">Week 02</a><br /><sup>My WebDen</sup></li>
+                <li><a href="week03/index.html">Week 03</a><br /><sup>E-Commerce</sup></li>
+                <li><a href="week04/index.html">Week 04</a></li>
+                <li><a href="week05/index.html">Week 05</a></li>
+                <li><a href="week06/index.html">Week 06</a></li>
+                <li><a href="week07/index.html">Week 07</a></li>
+                <li><a href="week08/index.html">Week 08</a></li>
+                <li><a href="week09/index.html">Week 09</a></li>
+                <li><a href="week10/index.html">Week 10</a></li>
+                <li><a href="week11/index.html">Week 11</a></li>
+                <li><a href="week12/index.html">Week 12</a></li>
+                <li><a href="week13/index.html">Week 13</a></li>
             </ul>
             <hr />
             <ul class="center-text">

8c4c30380c0f51ccacb2ddbbec620e56cc4b4bf5
James D. Downer <dow16002@byui.edu>
2018-10-06 14:54:50 -0600

    Redo Top.CSS; add skeleton for Week03

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     </head>
     <body>
+        <div class="meta-wrapper">
         <div class="banner-top center-text fill">
             <span class="header">CS 213 Assignments</span>
             <br />
@@ -14,25 +15,27 @@
         </div>
         <div class="content-top fill">
             <ul class="center-text">
-                <li><a href="week02/index.html">Week 02</a><br /><sup>My WebDen</sup></li>
-                <li><a href="week03/index.html">Week 03</a><br /><sup>E-Commerce</sup></li>
-                <li><a href="week04/index.html">Week 04</a></li>
-                <li><a href="week05/index.html">Week 05</a></li>
-                <li><a href="week06/index.html">Week 06</a></li>
-                <li><a href="week07/index.html">Week 07</a></li>
-                <li><a href="week08/index.html">Week 08</a></li>
-                <li><a href="week09/index.html">Week 09</a></li>
-                <li><a href="week10/index.html">Week 10</a></li>
-                <li><a href="week11/index.html">Week 11</a></li>
-                <li><a href="week12/index.html">Week 12</a></li>
-                <li><a href="week13/index.html">Week 13</a></li>
+                <li><a href="week02/index.html"><div class="button">Week 02<br /><sup>My WebDen</sup></div></a></li>
+                <li><a href="week03/index.html"><div class="button">Week 03<br /><sup>E-Commerce</sup></div></a></li>
+                <li><a href="week04/index.html"><div class="button">Week 04</div></a></li>
+                <li><a href="week05/index.html"><div class="button">Week 05</div></a></li>
+                <li><a href="week06/index.html"><div class="button">Week 06</div></a></li>
+                <li><a href="week07/index.html"><div class="button">Week 07</div></a></li>
+                <li><a href="week08/index.html"><div class="button">Week 08</div></a></li>
+                <li><a href="week09/index.html"><div class="button">Week 09</div></a></li>
+                <li><a href="week10/index.html"><div class="button">Week 10</div></a></li>
+                <li><a href="week11/index.html"><div class="button">Week 11</div></a></li>
+                <li><a href="week12/index.html"><div class="button">Week 12</div></a></li>
+                <li><a href="week13/index.html"><div class="button">Week 13</div></a></li>
             </ul>
-            <hr />
+        </div>
+        <div class="content-top fill">
             <ul class="center-text">
-                <li><a href="https://byui-cs213-downerj.herokuapp.com">CS 213</a><br />Heroku</li>
-                <li><a href="https://byui-cs313-downerj.herokuapp.com">CS 313</a><br />Heroku</li>
-                <li><a href="https://ascenderx.github.io">My Github.io</a><br />Github</li>
+                <li><a href="https://byui-cs213-downerj.herokuapp.com"><div class="button">CS 213<br /><sup>Heroku</sup></div></a></li>
+                <li><a href="https://byui-cs313-downerj.herokuapp.com"><div class="button">CS 313<br /><sup>Heroku</sup></div></a></li>
+                <li><a href="https://ascenderx.github.io"><div class="button">My Github.io<br /><sup>Github</sup></div></a></li>
             </ul>
+        </div>
         </div>
     </body>
 </html>

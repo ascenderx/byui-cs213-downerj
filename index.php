d2ebd3fe51dbd3de2a4cc50403d3d0fcf6699175
downerj <downer.james.256k@gmail.com>
2018-10-12 22:30:26 -0600

    Index: Add week04 description

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             <ul class="center-text">
                 <li><a href="week02/index.html"><div class="button">Week 02<br /><sup>My WebDen</sup></div></a></li>
                 <li><a href="week03/index.html"><div class="button">Week 03<br /><sup>E-Commerce</sup></div></a></li>
-                <li><a href="week04/index.html"><div class="button">Week 04</div></a></li>
+                <li><a href="week04/index.html"><div class="button">Week 04<br /><sup>E-Commerce v2</sup></div></a></li>
                 <li><a href="week05/index.html"><div class="button">Week 05</div></a></li>
                 <li><a href="week06/index.html"><div class="button">Week 06</div></a></li>
                 <li><a href="week07/index.html"><div class="button">Week 07</div></a></li>

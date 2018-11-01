c4cfbce179c4527de0767092ee7bf2e189eee5d3
downerj <downer.james.256k@gmail.com>
2018-11-01 15:12:45 -0600

    Week07: Make index.html link to browse.html

--- week07/resourcemanage.js
+++ week07/resourcemanage.js
@@ -2,7 +2,7 @@ function loadProducts(fileName) {
     let promise = ajaxGet(fileName);
 
     return {
-        forEach: function(callback) {
+        each: function(callback) {
             promise.then((json) => {
                 let products = JSON.parse(json);
                 for (let product of products) {
@@ -12,10 +12,37 @@ function loadProducts(fileName) {
                 console.log(`${status}: ${text}`);
                 callback(null);
             });
+        },
+        all: function(callback) {
+            promise.then((json) => {
+                let products = JSON.parse(json);
+                callback(products);
+            }).catch((status, text) => {
+                console.log(`${status}: ${text}`);
+                callback(null);
+            });
         }
     }
 }
 
 function loadCart() {
-    
+    let storage = new StorageManager();
+    let itemsJSON = storage.getItem('cart-items');
+    if (itemsJSON === null) {
+        return new ShoppingCart();
+    }
+
+    let items = JSON.parse(itemsJSON);
+    let cart = new ShoppingCart(items);
+    return cart;
+}
+
+function saveCart(cart) {
+    let storage = new StorageManager();
+    storage.addItem('cart-items', cart.products);
+}
+
+function removeCartItemsFromStorage() {
+    let storage = new StorageManager();
+    storage.removeItem('cart-items');
 }

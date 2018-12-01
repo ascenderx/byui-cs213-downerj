abbbcc0c749dc2392b7d0ebd67422af1908d3f87
James Downer <dow16002@byui.edu>
2018-12-01 14:33:12 -0700

    Week11: Add prob#3

---
+++ week11/problem3.php
@@ -0,0 +1,13 @@
+<?php
+  $input3 = htmlspecialchars($_POST["input3"]);
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Week 11 : Problem 3</title>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <p><?= $input3 ?></p>
+  </body>
+</html>
